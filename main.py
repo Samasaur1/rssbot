accc7e5d5f0edbae121d1b9e69909db31f40672f
Sam <30577766+Samasaur1@users.noreply.github.com>
2023-03-03 00:13:56 -0800

    Use 'verbose' to not over-log

--- main.py
+++ main.py
@@ -24,22 +24,22 @@ class Entry:
         self.content_hash = hashlib.md5(entry["content"][0].value.encode()).hexdigest() if "content" in entry else None
 
     def __eq__(self, other):
-        print("in __eq__")
+        verbose("in __eq__")
         if not other:
             return False
         if self.id and other.id:
-            print(f"match by id: {self.id == other.id}")
+            verbose(f"match by id: {self.id == other.id}")
             return self.id == other.id
         if self.link and other.link:
-            print(f"match by link: {self.link == other.link}")
+            verbose(f"match by link: {self.link == other.link}")
             return self.link == other.link
         if self.title and other.title:
-            print(f"match by title: {self.title == other.title}")
+            verbose(f"match by title: {self.title == other.title}")
             return self.title == other.title
         if self.summary_hash and other.summary_hash:
-            print(f"match by s_h: {self.summary_hash == other.summary_hash}")
+            verbose(f"match by s_h: {self.summary_hash == other.summary_hash}")
             return self.summary_hash == other.summary_hash
-        print(f"match by c_h: {self.content_hash == other.content_hash}")
+        verbose(f"match by c_h: {self.content_hash == other.content_hash}")
         return self.content_hash == other.content_hash
 
     def output(self):
@@ -58,51 +58,51 @@ class FeedData:
         self.previous_entry: Optional[Entry] = None #The most recent "nwewst entry" — e.g., entries[0] from the last time we fetched the feed
 
     def new_entries(self):
-        print("in new_entries")
+        verbose("in new_entries")
         if self.previous_entry:
-            print(f"previous entry {self.previous_entry.title}")
+            verbose(f"previous entry {self.previous_entry.title}")
         else:
-            print("no prev entry")
+            verbose("no prev entry")
         if self.etag or self.modified:
-            print("etag/modified")
+            verbose("etag/modified")
             d = feedparser.parse(self.url, etag=self.etag, modified=self.modified)
             # if self.etag:
             #     d = feedparser.parse(self.url, etag=self.etag)
             # else:
             #     d = feedparser.parse(self.url, modified=self.modified)
             if d.status == 304:
-                print("status 304")
+                verbose("status 304")
                 return []
         else:
-            print("parsing normally")
+            verbose("parsing normally")
             d = feedparser.parse(self.url)
 
-        print("d has been parsed")
+        verbose("d has been parsed")
         if "etag" in d:
             if d.etag != self.etag:
-                print("new etag")
+                verbose("new etag")
                 self.etag = d.etag
         if "modified" in d:
             if d.modified != self.modified:
-                print("new modified")
+                verbose("new modified")
                 self.modified = d.modified
 
         new_entries = []
         for _entry in d.entries:
-            print(f"entry {_entry.get('title', _entry.get('link', '???'))}")
+            verbose(f"entry {_entry.get('title', _entry.get('link', '???'))}")
             entry = Entry(_entry)
-            print(f"entry class {entry}")
+            verbose(f"entry class {entry}")
             if entry == self.previous_entry:
-                print("equal")
+                verbose("equal")
                 break
-            print("adding to list")
+            verbose("adding to list")
             new_entries.append(entry)
 
         if len(new_entries) == 0:
             return []
 
         self.previous_entry = new_entries[0]
-        print(self.previous_entry)
+        verbose(self.previous_entry)
         return new_entries
 
 
@@ -217,10 +217,10 @@ To list all feeds in this channel, try "<@1080989856248893521> list"
                     if feed not in self.feed_data:
                         self.feed_data[feed] = FeedData(feed)
                         self.feed_data[feed].new_entries()
-                        continue # Assume that newly-added blogs have had all their posts read already
+                        # Assume that newly-added blogs have had all their posts read already
                     entries = self.feed_data[feed].new_entries()
-                    print(len(entries))
-                    print([e.title for e in entries])
+                    for entry in entries:
+                        print(f"New post on {feed}: {entry.output()}")
 
                     for channel_id in self.feeds[feed]:
                         channel = self.get_channel(channel_id)
@@ -250,90 +250,11 @@ To list all feeds in this channel, try "<@1080989856248893521> list"
             # self.schedule_oob(channel_id)
 
         if self.task:
-            print("Canceling task")
+            verbose("Canceling task")
             self.task.cancel()
 
         delay_task = create_task(task(), name="rssbot.update")
-        # delay_task = create_task(task(), name=f"oob_delay_fn.{channel_id}.{delay_secs}")
         self.task = delay_task
-        # self.scheduled_oobs[channel_id]["delay_task"] = delay_task
-
-#
-#     def schedule_oob(self, channel_id: int) -> None:
-#         """Schedule an oob to be sent in the future.
-#
-#         This will replace any existing scheduled oob for this channel. The delay
-#         will be in the range of [0.5 * self.delay_secs, self.delay_secs)."""
-#
-#         channel = self.get_channel(channel_id)
-#         channel_desc = f"#{channel.name} in {channel.guild.name}"
-#
-#         delay_task = self.scheduled_oobs[channel_id]["delay_task"]
-#         delay_secs = self.scheduled_oobs[channel_id]["delay_secs"]
-#
-#         # If there is already an oob scheduled, cancel it.
-#         if delay_task:
-#             verbose(f"cancelling existing delay task '{delay_task.get_name()}'")
-#             delay_task.cancel()
-#
-#         # Randomize the delay based on self.delay_secs.
-#         delay_secs = max(self.DELAY_MIN, int(randrange(delay_secs // 2, delay_secs)))
-#
-#         # Create a task that waits delay seconds before calling oob().
-#         async def task():
-#             await sleep(delay_secs)
-#
-#             # While unlikely, it is possible that schedule_oob() could be called
-#             # while the current task is in the middle of running oob() (since
-#             # it has a small delay to simulate typing). Running oob() in a new
-#             # task should prevent this.
-#             create_task(self.oob(channel, None))
-#
-#             # Reset the delay to the maximum and start a new delay task.
-#             # Restarting the task ensures that the bot will eventually send an
-#             # oob again even if no one else sends one.
-#             self.scheduled_oobs[channel_id]["delay_secs"] = self.DELAY_MAX
-#             self.schedule_oob(channel_id)
-#
-#         delay_task = create_task(task(), name=f"oob_delay_fn.{channel_id}.{delay_secs}")
-#         self.scheduled_oobs[channel_id]["delay_task"] = delay_task
-#         verbose(f"started new delay task '{delay_task.get_name()}'")
-#
-#         m, s = divmod(delay_secs, 60)
-#         h, m = divmod(m, 60)
-#         d, h = divmod(h, 24)
-#         verbose(
-#             f"next oob in {channel_desc} will be in {delay_secs}s",
-#             f"({d}d {h}h {m}m {s}s)",
-#         )
-#
-#     async def on_ready(self) -> None:
-#         """Called when the bot is ready to start."""
-#
-#         print(f"logged in as {self.user}!")
-#         await self.change_presence(status=Status.idle, activity=Game("oob"))
-#         for channel_id in self.scheduled_oobs.keys():
-#             self.schedule_oob(channel_id)
-#
-#     async def on_message(self, message: Message) -> None:
-#         """Called when a message is sent."""
-#
-#         # Never respond to our own messages.
-#         if message.author == self.user:
-#             return
-#
-#         # Respond immediately if the message is a DM or mentions us.
-#         if isinstance(message.channel, DMChannel) or self.user.mentioned_in(message):
-#             await self.oob(message.channel, message)
-#             return
-#
-#         # Otherwise, handle the message if it is in $DISCORD_CHANNEL.
-#         if message.channel.id in self.scheduled_oobs.keys():
-#             # Reduce the delay by DELAY_POW and start a new delayed oob task.
-#             self.scheduled_oobs[message.channel.id]["delay_secs"] = int(
-#                 self.scheduled_oobs[message.channel.id]["delay_secs"] ** self.DELAY_POW
-#             )
-#             self.schedule_oob(message.channel.id)
 
 
 if __name__ == "__main__":
